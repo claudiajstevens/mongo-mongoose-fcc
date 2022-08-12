ce2c631f2251d7ebd89b164f2bc491f7f6a13a16
Claudia Stevens <71283725+claudiajstevens@users.noreply.github.com>
2022-08-12 06:34:06 +0000

    Finished up all queries

--- myApp.js
+++ myApp.js
@@ -81,32 +81,82 @@ const findPersonById = (personId, done) => {
   });
 };
 
+
+  /*Modify the findEditThenSave function to find a person by _id (use any of the above methods) with the parameter personId as search key. Add "hamburger" to the list of the person's favoriteFoods (you can use Array.push()). Then - inside the find callback - save() the updated Person.
+
+Note: This may be tricky, if in your Schema, you declared favoriteFoods as an Array, without specifying the type (i.e. [String]). In that case, favoriteFoods defaults to Mixed type, and you have to manually mark it as edited using document.markModified('edited-field'). See our Mongoose article.*/
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, function(err, person){
+    if(err){
+      return console.error(err);
+    }
+    
+    person.favoriteFoods.push(foodToAdd);
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  });
 };
 
+/*Modify the findAndUpdate function to find a person by Name and set the person's age to 20. Use the function parameter personName as the search key.
+
+Note: You should return the updated document. To do that, you need to pass the options document { new: true } as the 3rd argument to findOneAndUpdate(). By default, these methods return the unmodified object.*/
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+  Person.findOne({name: personName}, function(err, person){
+    if(err){
+      return console.error(err);
+    }
 
-  done(null /*, data*/);
+    person.age = ageToSet;
+    person.save((err, updatedPerson) => {
+      if(err){
+        return console.log(err);
+      }
+      done(null, updatedPerson, {new: true});
+    })
+  });
 };
 
+/*Modify the removeById function to delete one person by the person's _id. You should use one of the methods findByIdAndRemove() or findOneAndRemove().*/
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, function(err, person){
+    if(err){
+      return console.error(err);
+    }
+    done(null, person);
+  });
 };
 
+/*Modify the removeManyPeople function to delete all the people whose name is within the variable nameToRemove, using Model.remove(). Pass it to a query document with the name field set, and a callback.
+
+Note: The Model.remove() doesn’t return the deleted document, but a JSON object containing the outcome of the operation, and the number of items affected. Don’t forget to pass it to the done() callback, since we use it in tests.*/
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name: nameToRemove}, function(err, removedData){
+    if(err){
+      return console.error(err);
+    }
+    done(null, removedData);
+  });
 };
 
+
+/*Modify the queryChain function to find people who like the food specified by the variable named foodToSearch. Sort them by name, limit the results to two documents, and hide their age. Chain .find(), .sort(), .limit(), .select(), and then .exec(). Pass the done(err, data) callback to exec().*/
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 'asc'})
+    .limit(2)
+    .select({"age":0})
+    .exec(function(err, data){
+    if(err){
+      return console.error(err);
+    }
+    done(null, data);
+  });
 };
 
 /** **Well Done !!**
